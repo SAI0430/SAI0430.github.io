a71ac841f38a01e60675063eae806bd1b7f55faf
SAI0430 <168064156+SAI0430@users.noreply.github.com>
2024-04-25 16:11:33 +0900

    Add files via upload

---
+++ js/vegas.js
@@ -0,0 +1,33 @@
+$(function() {
+    $('.mainimg-tate').vegas({
+        slides: [
+            { src: 'images/IMG_4871.jpeg' },
+            { src: 'images/IMG_6817.jpeg' },	//1枚目の写真指定
+            { src: 'images/IMG_8223.jpeg' },	//2枚目の写真指定
+            { src: 'images/IMG_8343.jpeg'}	,
+            { src: 'images/IMG_8344.jpeg'}	,
+            
+        ],
+		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		delay: 6000,				//次の画像を表示するまでの時間
+		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
+
+//なくても⚪︎つけるならHTML編集
+$(function() {
+    $('.mainimg-yoko').vegas({
+        slides: [
+            { src: './images/yoko_1.jpg' },	//1枚目の写真指定
+            { src: './images/yoko_2.jpg' },	//2枚目の写真指定
+            { src: './images/yoko_3.jpg' },	//3枚目の写真指定
+        ],
+		transition: 'blur',			//https://vegas.jaysalvat.com/documentation/transitions/から好みのtransitionを選んで置き換えられます。
+		animation: 'kenburns',		//https://vegas.jaysalvat.com/documentation/transitions/から好みのanimationを選んで置き換えられます。
+		delay: 6000,				//次の画像を表示するまでの時間
+		animationDuration: 10000,	//アニメーション間の引き継ぎタイミング。
+		timer: false,				//プログレスバーを非表示に。
+    });
+});
